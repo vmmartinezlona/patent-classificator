9d98e2754edb29ddeaf482129ef42d2c291aa20a
Jaime <j.loyolarangel@gmail.com>
2017-06-07 21:59:45 -0500

    Add comments to file

--- classifier.py
+++ classifier.py
@@ -12,43 +12,69 @@ from sklearn import metrics
 import numpy as np
 import time
 
-
-def read_data(file, data, label, label_names):
+"""
+Permite leer los archivos que contienen los datos a clasificar.
+Argumentos:
+    1. Archivo de datos
+    2. Corpus para guardar los datos
+    3. Lista para guardar los identificadores de etiquetas
+    4. Lista con las etiquetas de cada sección (A)
+    5. Lista con las etiquetas de cada clase (01)
+    6. Lista con etiquetas en formato completo A01
+"""
+def read_data(file, data, labels_list, section_labels, class_labels, label_names):
     with open(file) as content_file:
         for line in content_file:
             words = line.split()
-            text = ' '.join(word for word in words[1:] if len(word)>2 and len(word)>30)
+            text = ' '.join(word for word in words[2:] if len(word)>2 and len(word)<30)
             data.append(text)
-            for num in range(0,7):
-                if words[0]==label_names[num]:
-                    label.append(num)
+            # Construir la etiqueta a comparar por sección y clase
+            for letter in range(0, 8):
+                for num in range(0, 10):
+                    for num2 in range(0, 10):
+                        label = section_labels[letter] + class_labels[num] + class_labels[num2]
+                        if words[1]==label:
+                            label_names.append(label)
+                            labels_list.append(num)
 
+# Directorios
 main_dir = '/home/jaime/BACKUP/Docs/DataMining/project/'
 train_file = main_dir + 'patents_train.txt'
-#val_file = main_dir + 'patents_validation.txt'
 test_file = main_dir + 'patents_test.txt'
-label_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-corpus = []
-labels_list = []
+# # Para etiquetas (por sección y clase)
+section_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+class_labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+label_names = []
 
-read_data(train_file, corpus, labels_list, label_names)
+#________________________________________________________________ Datos de entrenamiento
+# # Variables para guardar los datos y las etiquetas para clasificar.
+train_corpus = []
+train_labels_list = []
+# # Leer datos de los archivos de entrenamiento.
+# # # Cargar corpus y listas de etiquetas (por nombre e identificador)
+read_data(train_file, train_corpus, train_labels_list, section_labels, class_labels, label_names)
 
-labels = np.asarray(labels_list)
+train_labels = np.asarray(train_labels_list)
 
-vec.TfidfVectorizer(stop_words='english', min_df=2, norm='l2')
-corpus_fit = vec.fit_transform(corpus)
+# # Preprocesamiento
+vec = TfidfVectorizer(stop_words='english', min_df=2, norm='l2')
+train_corpus_fit = vec.fit_transform(train_corpus)
 
+#________________________________________________________________ Datos de prueba
 test_corpus = []
 test_labels_list = []
-
-read_data(test_file, test_corpus, test_labels_list, label_names)
+# # Leer datos de los archivos de entrenamiento.
+# # # Cargar corpus y listas de etiquetas (por nombre e identificador)
+read_data(test_file, test_corpus, test_labels_list, section_labels, class_labels, label_names)
 test_labels = np.asarray(test_labels_list)
 
-test_fit = vec.transform(test_corpus)
+# # Preprocesamiento
+test_corpus_fit = vec.transform(test_corpus)
 
+#________________________________________________________________ Bayes
 start = time.time()
-clf_nb = MultinomialNB().fit(corpus_fit, labels)
-predicted_nb = clf_nb.predict(test_fit)
+classifier_nb = MultinomialNB().fit(train_corpus_fit, train_labels)
+predicted_nb = classifier_nb.predict(test_corpus_fit)
 stop = time.time()
 
 print('\n Accuracy = ' + str(np.mean(predicted_nb == test_labels)))
