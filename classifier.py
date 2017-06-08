8a6301a92bcf1c4ace3f9455c6df897602e99e2b
vmmartinezlona <vm.martinezlona@ugto.mx>
2017-06-08 00:54:19 -0500

    Creacion del archivo de salida

---
+++ classifier.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 11:29:15 2017
+
+@author: jaime
+"""
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import numpy as np
+import time
+
+"""
+Función que lee todas las etiquetas en el documento para crear una lista
+en la que no se repiten las etiquetas.
+
+return Lista de etiquetas (sección y clase)
+"""
+def get_labels(file):
+    labels_name = []
+    with open(file) as content_file:
+        for line in content_file:
+            words = line.split()
+            labels_name.append(words[1])
+    labels_name = list(set(labels_name))
+    return labels_name
+
+"""
+Permite leer los archivos que contienen los datos a clasificar.
+Argumentos:
+    1. Archivo de datos
+    2. Corpus para guardar los datos
+    3. Lista para guardar los identificadores de etiquetas
+    4. Lista con etiquetas en formato completo A01
+"""
+def read_data(file, data, labels_list, label_names):
+    with open(file) as content_file:
+        for line in content_file:
+            words = line.split()
+            text = ' '.join(word for word in words[2:] if len(word)>2 and len(word)<30)
+            data.append(text)
+            # Construir la etiqueta a comparar por sección y clase
+            i = 0
+            for label in label_names:
+                if words[1]==label:
+                    labels_list.append(i)
+                i = i  + 1
+    #print (labels_list)
+
+# Directorios
+main_dir = '/home/tsukimo/Documents/patent-classificator/'
+train_file = main_dir + 'patents_train.txt'
+test_file = main_dir + 'patents_test.txt'
+# # Para etiquetas (por sección y clase)
+train_labels_name = []
+
+#________________________________________________________________ Datos de entrenamiento
+# # Variables para guardar los datos y las etiquetas para clasificar.
+train_corpus = []
+train_labels_list = []
+# # Leer datos de los archivos de entrenamiento.
+# # # Cargar corpus y listas de etiquetas (por nombre e identificador)
+train_labels_name = get_labels(train_file)
+read_data(train_file, train_corpus, train_labels_list, train_labels_name)
+train_labels = np.asarray(train_labels_list)
+
+# # Preprocesamiento
+vec = TfidfVectorizer(stop_words='english', min_df=2, norm='l2')
+train_corpus_fit = vec.fit_transform(train_corpus)
+
+#________________________________________________________________ Datos de prueba
+test_corpus = []
+test_labels_list = []
+test_labels_name = []
+# # Leer datos de los archivos de entrenamiento.
+# # # Cargar corpus y listas de etiquetas (por nombre e identificador)
+test_labels_name = get_labels(test_file)
+read_data(test_file, test_corpus, test_labels_list, test_labels_name)
+test_labels = np.asarray(test_labels_list)
+
+# # Preprocesamiento
+test_corpus_fit = vec.transform(test_corpus)
+
+#________________________________________________________________ Bayes
+start = time.time()
+classifier_nb = MultinomialNB().fit(train_corpus_fit, train_labels)
+predicted_nb = classifier_nb.predict(test_corpus_fit)        
+stop = time.time()
+
+
+#Matriz de probabilidades
+#section_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+predicted_prob = classifier_nb.predict_proba(test_corpus_fit)
+num_test_file = predicted_nb.size
+#Crea el archivo de salida
+output_file = main_dir + 'output.txt'
+#Arreglo para guardar el identificador de patente
+Id = []
+#Obtener el identificador de patente de cada patente en test_file
+with open(test_file, 'r', encoding = 'UTF-8') as test:
+    for line in test:    
+        Id.append(line.split()[0])
+
+#Llenar el archivo de salida
+with open(output_file, 'w', encoding = 'UTF-8') as file:
+    for num in range(0, num_test_file):
+        # Obtener los indices de los 3 elementos mayores
+        sort_index = np.argsort(predicted_prob[num][:])[::-1][:3]
+        #Escribir la el identidicador de patente
+        file.write(str(Id[num]))
+        file.write(' ')
+        for num2 in sort_index:
+            prob = train_labels_name[num2]
+            file.write(prob)
+            file.write(' ')
+        file.write('\n')
+
+#________________________________________________________________ Escribir resultados
+#f = open("output.txt", "w")
+#f.write('Class: ' + names[index] + str(predicted_nb[index])+'\n')
